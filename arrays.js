8533b4923a1314af4ca3d8e9a089773de35073b2
magxalasinaye00-max <magxalasinaye00@gmail.com>
2025-10-31 10:40:43 +0200

    array in java

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let colours = []
+colours.push("red")
+colours.push("blue")
+colours.push("green")
+let numbers = [1, 2, 3, 4, 5]
+colours.pop()
+colours.unshift("yellow")
+console.log("length of colours array:", colours.length)
+console.log("length of numbers array:", numbers.length)
